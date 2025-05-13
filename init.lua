d6026e6168961043a485b4ac3e16f0c8e6eb7434
Rodrigo Cabrera <rodrigo.cabrera.catemu10@gmail.com>
2025-05-13 13:32:46 -0400

    feat: add copilot and copilot-chat plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("plugins.color-scheme")
+require("plugins.copilot")
+require("plugins.copilot-chat")
---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken",
+    opts = {},
+  },
+}
